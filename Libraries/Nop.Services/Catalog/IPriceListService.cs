ca7a2c9ddc9cbca7466be8ebecb49e78c5378a4e
Olha <olhadub08@gmail.com>
2026-05-31 14:43:16 +0300

    Created interface for pricelist service (IPriceListService) and its implementation PriceListService. Added Dependency Injection in NopStartup.cs.

---
+++ Libraries/Nop.Services/Catalog/IPriceListService.cs
@@ -0,0 +1,17 @@
+﻿using Nop.Core.Domain.Catalog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Services.Catalog
+{
+    public partial interface IPriceListService
+    {
+        Task InsertPriceListAsync(PriceList priceList);
+        Task UpdatePriceListAsync(PriceList priceList);
+        Task<PriceList> GetPriceListByIdAsync(int priceListId);
+        Task<IList<PriceListItem>> GetPriceListItemsByPriceListIdAsync(int priceListId);
+    }
+}
---
+++ Libraries/Nop.Services/Catalog/PriceListService.cs
@@ -0,0 +1,40 @@
+﻿using Nop.Core.Domain.Catalog;
+using Nop.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Services.Catalog
+{
+    public class PriceListService:IPriceListService
+    {
+        private readonly IRepository<PriceList> _repository;
+        private readonly IRepository<PriceListItem> _listItemRepository;
+        public PriceListService(IRepository<PriceList> repository, IRepository<PriceListItem> listItemRepository)
+        {
+            _repository = repository;
+            _listItemRepository = listItemRepository;
+        }
+        public async Task InsertPriceListAsync(PriceList priceList)
+        {
+            await _repository.InsertAsync(priceList);
+        }
+        public async Task UpdatePriceListAsync(PriceList priceList)
+        {
+            await _repository.UpdateAsync(priceList);
+            
+        }
+        public async Task<PriceList> GetPriceListByIdAsync(int priceListId)
+        {
+            var priceList=await _repository.GetByIdAsync(priceListId);
+            return priceList;
+        }
+        public async Task<IList<PriceListItem>>GetPriceListItemsByPriceListIdAsync(int priceListId)
+        {
+            var priceListItems = await _listItemRepository.GetAllAsync(query=>query.Where(p=> p.PriceListId==priceListId));
+            return priceListItems;
+        }
+    }
+}
