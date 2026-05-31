a78021dea51bdd666ede9cb3906db9071520b063
Olha <olhadub08@gmail.com>
2026-05-31 14:34:55 +0300

    Created configurations for Entity Framework(PriceListBuilder and PriceListItemBuilder) to describe tables

---
+++ Libraries/Nop.Data/Mapping/Builders/Catalog/PriceListBuilder.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator.Builders.Create.Table;
+using Nop.Core.Domain.Catalog;
+using Nop.Core.Domain.Directory;
+using Nop.Data.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Data.Mapping.Builders.Catalog
+{
+    public partial class PriceListBuilder : NopEntityBuilder<PriceList>
+    {
+        #region Methods
+        public override void MapEntity(CreateTableExpressionBuilder table)
+        {
+            table
+                .WithColumn(nameof(PriceList.Id)).AsInt32().NotNullable().PrimaryKey()
+                .WithColumn(nameof(PriceList.Name)).AsString(400).NotNullable()
+                .WithColumn(nameof(PriceList.CurrencyId)).AsInt32().NotNullable().ForeignKey(nameof(Currency), "Id");
+        }
+        #endregion
+    }
+}
---
+++ Libraries/Nop.Data/Mapping/Builders/Catalog/PriceListItemBuilder.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator.Builders.Create.Table;
+using Nop.Core.Domain.Catalog;
+using Nop.Data.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Data.Mapping.Builders.Catalog
+{
+    public partial class PriceListItemBuilder: NopEntityBuilder<PriceListItem>
+    {
+        #region Methods
+        public override void MapEntity(CreateTableExpressionBuilder table)
+        {
+            table
+                .WithColumn(nameof(PriceListItem.Id)).AsInt32().NotNullable().PrimaryKey()
+                .WithColumn(nameof(PriceListItem.PriceListId)).AsInt32().NotNullable().ForeignKey(nameof(PriceList), "Id").OnDelete(System.Data.Rule.Cascade)
+                .WithColumn(nameof(PriceListItem.ProductId)).AsInt32().NotNullable().ForeignKey(nameof(Product), "Id").OnDelete(System.Data.Rule.None)
+                .WithColumn(nameof(PriceListItem.Price)).AsDecimal(18,4).NotNullable();
+        }
+        #endregion
+    }
+}
